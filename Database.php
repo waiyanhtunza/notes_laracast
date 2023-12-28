e60cf2bac2331912f5df276e915e4c4c2b2b0207
waiyanhtunza <wyh@za.com.mm>
2023-12-28 10:12:19 +0630

    new function findorfial

--- Database.php
+++ Database.php
@@ -3,6 +3,7 @@
 class Database {
 
     public $connection;
+    public $statement;
 
     public function __construct($config ,$username = 'root',$password = '') {
 
@@ -18,10 +19,25 @@ class Database {
 
     public function query($query,$params = []){
         
-        $statment = $this->connection->prepare($query);
-        $statment->execute($params);
+        $this->statement = $this->connection->prepare($query);
+        $this->statement->execute($params);
 
-        return $statment;
+        return $this;
         
+    }
+
+    public function find(){
+       return $this->statement->fetch();
+
+    }
+
+    public function findOrFail(){
+        $result= $this->find();
+
+        if($result){
+            abort();
+        }
+
+        return $result;
     }
 }
--- controllers/note.php
+++ controllers/note.php
@@ -9,19 +9,16 @@ $heading = 'My Notes';
 
 $note = $db->query('select * from notes where id = :id', [
     'id' => $_GET[ 'id']
-    ])->fetch();
+    ])->findOrFail();
 
 // dd($notes);
-if (!$note){
-    abort();
-}
 
 $currentId = 2;
 $forbidden = 403;
 
 
 if ($note['user_id'] != $currentId){
-    abort($forbidden);
+    abort(Response::FORBIDDEN);
 }
 
 
