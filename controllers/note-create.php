0c9ec6ebebd48e531c908b8cafeedaae1f270ada
waiyanhtunza <wyh@za.com.mm>
2023-12-29 10:49:31 +0630

    all validator

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -1,17 +1,28 @@
 <?php
+require "Validator.php";
 
 $config = require "config.php";
-
 $db = new Database($config['database']);
 
 
 $heading = 'Create New Note';
 
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
-    $db->query('INSERT INTO notes (body,user_id) VALUES (:body,:user_id)',[
-        'body'=> $_POST['body'],
-        'user_id'=> 3,
-    ]);
+
+    $error = [];
+
+    if(!Validator::string($_POST['body'],1,1000)){
+            $error['body'] = 'A body is required';
+    }
+    
+    if(empty($error)){
+        $db->query('INSERT INTO notes (body,user_id) VALUES (:body,:user_id)',[
+            'body'=> $_POST['body'],
+            'user_id'=> 3,
+        ]);
+    }
+   
+    
     
 }
 
