0fc9cfd35d42d51a2383fa934161e15ec1a14eac
waiyanhtunza <wyh@za.com.mm>
2023-12-29 15:28:57 +0630

    every thing is ok par

--- public/index.php
+++ public/index.php
@@ -1,27 +1,17 @@
 <?php
 
+use Core\Database;
 const BASE_PATCH = __DIR__ . "/../";
 
 require BASE_PATCH . "Core/functions.php";
 
 spl_autoload_register(function ($class) {
-
+    // dd($class);
     $class = str_replace("\\",DIRECTORY_SEPARATOR, $class);
+    
     require base_path("{$class}.php");
 });
 
+// require base_path("Core/Database.php")
 require base_path("Core/router.php");
-//connect to database;
-
-
-
-// $db = new Database();
-// $posts = $db->query("SELECT * FROM posts")->fetchAll(PDO::FETCH_ASSOC);
-
-
-
-
-// $id = $_GET['id'];
-// $query = "SELECT * FROM posts WHERE id = :id ";  
-// $posts = $db->query($query,[':id' => $id])->fetchAll();
 
